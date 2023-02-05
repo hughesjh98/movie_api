60afcc845a97b5428cd4a94404a84ced412f1865
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2023-02-05 14:48:25 +0700

    update connect

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const passport = require('passport');
 require('./passport');
 const { check, validationResult } = require('express-validator');
 
-let allowOrgins = ['http://localhost:8080', 'http://testsite.com'];
+let allowOrgins = ['http://localhost:8080', 'http://testsite.com', 'https://movie-dash.herokuapp.com'];
 
 app.use(cors({
     origin: (origin, callback) =>{
