49b6157e5b766b23affdc3c64a39ca7a465cd773
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2023-02-05 16:13:20 +0700

    update connect

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User;
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', {
 //     useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
