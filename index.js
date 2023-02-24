59e45ceea4f3c5380dc9d34980fa48f815ab9d1b
hughesjh98 <89598311+hughesjh98@users.noreply.github.com>
2023-02-24 17:55:18 +0700

    add passport to /movies

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', {session: false}),  
   });
 
 //READ ALL OF THE MOVIES IN THE DATABASE
-app.get('/movies', async (req, res) => {
+app.get('/movies',passport.authenticate('jwt', {session: false}), async (req, res) => {
     try {
         const movies = await Movies.find()
 
